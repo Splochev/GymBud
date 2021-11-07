692d677c12e90a3c53cf102da098fc668bf2acd3
Stanislav Plochev <stanislav.plochev@gmail.com>
2021-11-07 12:28:25 +0200

    addRowBtn function added

--- client/src/components/ProgressTracker/ProgressTracker.js
+++ client/src/components/ProgressTracker/ProgressTracker.js
@@ -1,11 +1,12 @@
 import MUIDataTable from "mui-datatables";
 import useStyles from './styles'
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { useState } from "react";
 
 const columns = [
     {
-        name: "date",
-        label: "Date",
+        name: "week",
+        label: "Week",
         options: {
             sort: false,
         }
@@ -93,331 +94,9 @@ const columns = [
     },
 ];
 
-const data = [
-    {
-        date: "09/27/2021 - 10/03/2021",
-        1: '68',
-        2: '67.3',
-        3: '67.3',
-        4: '67.3',
-        5: '67.3',
-        6: '67.7',
-        7: '67.7',
-        avgWeight: '67.5',
-        weightChange: '0.00%',
-        id: '1'
-    },
-    {
-        date: "10/04/2021 - 10/10/2021",
-        1: '67.7',
-        2: '67.3',
-        3: '67.1',
-        4: '67.0',
-        5: '66.6',
-        6: '',
-        7: '66.4',
-        avgWeight: '67.0',
-        weightChange: '-0.74%',
-        id: '2'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    }, {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-    {
-        date: "10/11/2021 - 10/17/2021",
-        1: '',
-        2: '67.1',
-        3: '',
-        4: '',
-        5: '',
-        6: '',
-        7: '',
-        avgWeight: '67.1',
-        weightChange: '0.12%',
-        id: '28105'
-    },
-];
 
-const options = {
-    print: false,
-    responsive: 'simple',
-    page: 0,    // comes from server
-    count: data.length, //comes from erver
-    rowsPerPage: 7,
-    rowsPerPageOptions: [],
-    tableBodyHeight: '450px',
-    search: false,
-    filterType: 'multiselect',
-};
+
+
 
 const getMuiTheme = () => createTheme({
     overrides: {
@@ -456,31 +135,118 @@ const getMuiTheme = () => createTheme({
                 color: 'black'
             },
         },
-        MuiSelect: {
-            root: {
-                width: '250px'
-            }
-        },
     }
 })
 
-
-
 const ProgressTracker = () => {
     const classes = useStyles();
+    const [data, setData] = useState([
+        {
+            week: "Week 1",
+            1: '68',
+            2: '67.3',
+            3: '67.3',
+            4: '67.3',
+            5: '67.3',
+            6: '67.7',
+            7: '67.7',
+            avgWeight: '67.5',
+            weightChange: '0.00%',
+            id: '1'
+        },
+        {
+            week: "Week 2",
+            1: '67.7',
+            2: '67.3',
+            3: '67.1',
+            4: '67.0',
+            5: '66.6',
+            6: '',
+            7: '66.4',
+            avgWeight: '67.0',
+            weightChange: '-0.74%',
+            id: '2'
+        },
+        {
+            week: "Week 3",
+            1: '',
+            2: '67.1',
+            3: '',
+            4: '',
+            5: '',
+            6: '',
+            7: '',
+            avgWeight: '67.1',
+            weightChange: '0.12%',
+            id: '28105'
+        },
+        {
+            week: "Week 4",
+            1: '',
+            2: '67.1',
+            3: '',
+            4: '',
+            5: '',
+            6: '',
+            7: '',
+            avgWeight: '67.1',
+            weightChange: '0.12%',
+            id: '28105'
+        },
+        {
+            week: "Week 5",
+            1: '',
+            2: '67.1',
+            3: '',
+            4: '',
+            5: '',
+            6: '',
+            7: '',
+            avgWeight: '67.1',
+            weightChange: '0.12%',
+            id: '28105'
+        },
+    ])
+
+    function addNewRow(e) {
+        setData([...data, {
+            week: `Week ${data.length + 1}`,
+            1: '',
+            2: '',
+            3: '',
+            4: '',
+            5: '',
+            6: '',
+            7: '',
+            avgWeight: '0',
+            weightChange: '0.00%',
+            id: `${data.length + 1}`
+        }])
+    }
+
+    const options = {
+        print: false,
+        responsive: 'simple',
+        page: data.length,
+        rowsPerPage: 7,
+        rowsPerPageOptions: [],
+        tableBodyHeight: '450px',
+        search: false,
+        filterType: 'checkbox',
+    };
 
     return (
         <div className={classes.container}>
             <ThemeProvider theme={getMuiTheme()}>
                 <MUIDataTable
-                    title={"Employee List"}
+                    title={"Weight Tracker"}
                     data={data}
                     columns={columns}
                     options={options}
                     serverSide={true}
                 />
             </ThemeProvider>
-            <button data-toggle="tooltip" title="Add new row" className={classes.addButton}><i class={"fas fa-plus-circle " + classes.icon}></i></button>
+            <button data-toggle="tooltip" title="Add a new row" onClick={addNewRow} className={classes.addButton}><i class={"fas fa-plus-circle " + classes.icon}></i></button>
         </div >
     );
 }
