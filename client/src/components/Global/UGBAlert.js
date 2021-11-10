b20f0675a0ef349384606785df88357cb0f2e30d
Stanislav Plochev <stanislav.plochev@gmail.com>
2021-11-10 11:29:32 +0200

    add alert component

---
+++ client/src/components/Global/UGBAlert.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+  alert: {
+    background: '#DC3545'
+  }
+}));
+
+export default function UGBAlert({ open, setOpen, message }) {
+  const classes = useStyles();
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} classes={{ root: classes.alert }} severity="error">
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
