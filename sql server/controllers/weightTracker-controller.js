45c110e5d9edcfe9b00ef73b105fe439e31b0f3a
Stanislav Plochev <stanislav.plochev@gmail.com>
2022-01-23 19:22:40 +0200

    refactor WeightTrackerController get-weight-data

--- sql server/controllers/weightTracker-controller.js
+++ sql server/controllers/weightTracker-controller.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const MysqlAdapter = require('../utils/mysql-adapter');
 const { escape } = require('mysql');
+const ErrorHandler = require('../utils/error-handler');
+const AuthHelpers = require('../utils/auth-helpers');
 
 module.exports = class WeightTrackerController {
     constructor() {
@@ -8,22 +10,21 @@ module.exports = class WeightTrackerController {
         this.attachEndpoints();
     }
     attachEndpoints() {
-        this.router.get('/get-weight-data', (req, res) => this.getWeightData(req, res));
+        this.router.get('/get-weight-data', AuthHelpers.loggedIn,(req, res) => this.getWeightData(req, res));
     }
 
     async getWeightData(req, res) {
         try {
-
-            let weightData = await MysqlAdapter.query(`
+            const user = req.user;
+            const weightData = await MysqlAdapter.query(`
                 SELECT *
-                FROM weight_tracker;
+                FROM weight_tracker
+                WHERE user_id = ${escape(user.id)};
             `);
 
             res.json({ data: weightData });
-            
         } catch (error) {
-            console.error(error);
-            // res.status(500).json(ErrorHandler.GenerateError(500, ErrorHandler.ErrorTypes.server_error, 'Server error!'));
+            res.status(500).json(ErrorHandler.GenerateError(500, ErrorHandler.ErrorTypes.server_error, 'Server error!'));
         }
     };
 
