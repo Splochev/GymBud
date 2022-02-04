090e44e991d82f3f44cc14e0f6a4627e257c9a78
Stanislav Plochev <stanislav.plochev@gmail.com>
2022-02-05 01:20:55 +0200

    set up fetching from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoggedOutHeader from "./components/header/LoggedOutHeader/LoggedOutHeader"
 import LoggedInHeader from "./components/header/LoggedInHeader/LoggedInHeader";
 import HomePageArticles from "./components/HomePageArticles/HomePageArticles";
 import Footer from "./components/Footer/Footer";
 import ProgressTracker from './components/ProgressTracker/ProgressTracker'
 import { StoreContext, initialStoreState } from './components/store/Store';
-
 import { Route, Redirect } from 'react-router';
 import { Switch, BrowserRouter } from 'react-router-dom';
-
+import { getData } from './components/utils/FetchUtils'
+require("dotenv").config();
 
 
 const App = () => {
     const [storeState, setStoreState] = useState(initialStoreState);
+    const [isFetchAttached, setIsFetchAttached] = useState(false);
+
+    useEffect(() => {
+        const originalFetch = window.fetch;
+        window.fetch = function (input, init) {
+            // console.log('fetch', input);
+            return originalFetch(input, init)
+                .then(async resp => {
+                    if (!resp.ok) {
+                        const text = await resp.text();
+
+                        let parsedError = '';
+                        try {
+                            parsedError = JSON.parse(text).msg;
+                        } catch {
+                            parsedError = text;
+                        }
+
+                        switch (resp.status) {
+                            case 401:
+                                setStoreState(state => (state.user = null, { ...state }));
+                                // $snackbarMessage.set(parsedError);
+                                return;
+                            default:
+                                break;
+                        }
+
+                        throw new Error(parsedError);
+                    }
+
+                    // parses JSON response into native JavaScript objects
+                    return resp.json()
+                        .catch(err => console.error('response parse error', input, 'error', err));
+                },
+                    err => {
+                        // if fetch throws an error
+                        //   $snackbarMessage.set(err.message);
+                        console.error('fetch error', err);
+                    });
+        }
+
+        setIsFetchAttached(true);
+
+        getData(process.env.REACT_APP_HOST + '/api/user/me')
+            .then(data => {
+                setStoreState(state => (state.user = data, { ...state }))
+            }, error => {
+                console.log('api/user/me:', error)
+            });
+
+        return () => {
+            window.fetch = originalFetch;
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
-        <StoreContext.Provider value={[storeState, setStoreState]}>
-
-            <BrowserRouter>
-                {storeState.user || sessionStorage.getItem('user') ?
-                    <div>
-                        <LoggedInHeader />
+        isFetchAttached ?
+            <StoreContext.Provider value={[storeState, setStoreState]}>
+                <BrowserRouter>
+                    {storeState.user || sessionStorage.getItem('user') ?
                         <div>
-                            <Switch>
-                                <Route exact path="/home" component={HomePageArticles} />
-                                <Route exact path="/progress-tracker" component={ProgressTracker} />
-                                <Redirect from="*"  to="/home" />
-                            </Switch>
+                            <LoggedInHeader />
+                            <div>
+                                <Switch>
+                                    <Route exact path="/home" component={HomePageArticles} />
+                                    <Route exact path="/progress-tracker" component={ProgressTracker} />
+                                    <Redirect from="*" to="/home" />
+                                </Switch>
+                            </div>
+                            <Footer />
                         </div>
-                        <Footer />
-                    </div>
-                    :
-                    <div>
-                        <LoggedOutHeader />
+                        :
                         <div>
-                            <Switch>
-                                <Route exact path="/home" component={HomePageArticles} />
-                                <Redirect from="*" to="/home" />
-                            </Switch>
+                            <LoggedOutHeader />
+                            <div>
+                                <Switch>
+                                    <Route exact path="/home" component={HomePageArticles} />
+                                    <Redirect from="*" to="/home" />
+                                </Switch>
+                            </div>
+                            <Footer />
                         </div>
-                        <Footer />
-                    </div>
-                }
-            </BrowserRouter>
-        </StoreContext.Provider>
+                    }
+                </BrowserRouter>
+            </StoreContext.Provider>
+            :
+            <div>404 Not Found</div>
     );
 }
 
---
+++ client/src/components/utils/FetchUtils.js
@@ -0,0 +1,75 @@
+
+function makeFetchData(method, data) {
+    // Default options are marked with *
+    return {
+        method: method, // *GET, POST, PUT, DELETE, etc.
+        mode: 'same-origin', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: data ? JSON.stringify(data) : null // body data type must match "Content-Type" header
+    }
+}
+
+function fetchData(url, request) {
+    return fetch(url, request);
+}
+
+export function getData(url = '') {
+    return fetchData(url, makeFetchData('GET'));
+}
+
+export function postData(url = '', data = {}) {
+    return fetchData(url, makeFetchData('POST', data));
+}
+
+export function patchData(url = '', data = {}) {
+    return fetchData(url, makeFetchData('PATCH', data));
+}
+
+export function putData(url = '', data = {}) {
+    return fetchData(url, makeFetchData('PUT', data));
+}
+
+export function deleteData(url = '', data = {}) {
+    return fetchData(url, makeFetchData('DELETE', data));
+}
+
+
+
+// postData('https://example.com/answer', { answer: 42 })
+//     .then(data => {
+//         console.log(data); // JSON data parsed by `data.json()` call
+//     });
+
+export function randomInt(minOrMaxValue, maxValue) {
+    if (!maxValue) {
+        return Math.floor(Math.random() * minOrMaxValue);
+    }
+
+    return Math.floor(minOrMaxValue + Math.random() * (maxValue - minOrMaxValue));
+}
+
+export function debounce(onInput, func, wait = 300) {
+    let timeout;
+    function debounced(...args) {
+        const later = () => {
+            func.apply(this, args);
+        };
+
+        clearTimeout(timeout);
+        onInput.apply(this, args);
+        timeout = setTimeout(later, wait);
+    }
+
+    debounced.clear = () => {
+        clearTimeout(timeout);
+    };
+
+    return debounced;
+}
