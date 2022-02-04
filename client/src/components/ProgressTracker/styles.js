7d54b2350a072728cc28eef7f4604fbf2bed1e13
Stanislav Plochev <stanislav.plochev@gmail.com>
2022-02-05 00:50:28 +0200

    comment certain styles for now

--- client/src/components/ProgressTracker/styles.js
+++ client/src/components/ProgressTracker/styles.js
@@ -6,32 +6,33 @@ export default makeStyles(() => ({
             width: '89px',
         },
         "& .MuiFormLabel-root": {
-            color: "#868686"
+            // color: "#868686"
         },
         "& .MuiInputBase-input": {
             minWidth: '45px',
         },
         "& .MuiInput-underline": {
             '&::before': {
-                transition: 'none',
-                borderBottom: '1px solid #868686 !important'
+                // transition: 'none',
+                // borderBottom: '1px solid #868686 !important'
             },
             '&::after': {
-                transition: 'none',
-                borderBottom: '1px solid #868686 !important'
+                // transition: 'none',
+                // borderBottom: '1px solid #868686 !important'
             },
         },
     },
     muiInputAdornmentRoot: {
         "& .MuiButtonBase-root": {
             '&:hover': {
-                backgroundColor: 'none',
-                background: 'none'
+                // backgroundColor: 'none',
+                // background: 'none'
             }
         }
     },
     weightSubmission: {
         display: 'flex',
-        alignItems:'baseline'
-    }
+        alignItems: 'baseline',
+        
+    },
 }))
