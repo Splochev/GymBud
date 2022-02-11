169c313e58b39521c8c8f96acaaaec016be64d83
Stanislav Plochev <stanislav.plochev@gmail.com>
2022-02-11 15:18:37 +0200

    restyle link

--- client/src/components/OneRepMaxCalculator/OneRepMaxCalculator.js
+++ client/src/components/OneRepMaxCalculator/OneRepMaxCalculator.js
@@ -10,11 +10,18 @@ const useStyles = makeStyles((theme) => ({
     },
     icon: {
         fontSize: '20px'
-    }
+    },
+    link: {
+        color: '#007BFF',
+        '&:hover': {
+            textDecoration: 'underline',
+            color: '#007BFF'
+        }
+    },
 }));
 
 const OneRepMaxCalculator = () => {
-    const classes = useStyles();
+    const styles = useStyles();
     const [alert, setAlert] = useState('');
     const weight = useState(undefined);
     const reps = useState(undefined);
@@ -41,12 +48,17 @@ const OneRepMaxCalculator = () => {
                 One of its key benefits is that it lets you find out how much weight you can
                 maximally lift for one repetition.Knowing your 1RM is useful when building
                 your workout program because it makes adjusting the intensity simpler.
-                <br></br><br></br>
-                There are many different formulas to estimate your 1RM.The most popular and <a target='_blank'
+                <br /><br />
+                There are many different formulas to estimate your 1RM.The most popular and&nbsp;
+                <a
+                    className={styles.link}
+                    target='_blank'
                     rel='noreferrer'
-                    href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3525823/'>
-                    critically aclaimed
-                </a> is the Matt Brzycki formula which this calculator is using.
+                    href='https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3525823/'
+                >
+                    critically acclaimed
+                </a>
+                &nbsp;is the Matt Brzycki formula which this calculator is using.
                 The higher the number of reps the more unreliable the 1 Rep Max estimation will be.
                 Going above 10 repetitions is not recommended.The golden range for the formula is between 4-6
                 repetitions.
@@ -72,11 +84,11 @@ const OneRepMaxCalculator = () => {
                 </div>
                 <div className='form-group d-flex justify-content-center row'> Your one rep max is: </div>
                 <div className='form-group d-flex justify-content-center row'>
-                    <input type='number' value={oneRM[0]} className={clsx('form-control', classes.oneRepMaxInput)} name='oneRMResult' disabled />
+                    <input type='number' value={oneRM[0]} className={clsx('form-control', styles.oneRepMaxInput)} name='oneRMResult' disabled />
                 </div>
                 <div className='d-flex justify-content-center row'>
                     <button type='submit' className='btn btn-success' data-toggle='tooltip' title='Calculate 1 Rep Max' >
-                        <i className={clsx('fas fa-calculator', classes.icon)}></i>
+                        <i className={clsx('fas fa-calculator', styles.icon)}></i>
                     </button>
                 </div>
             </form>
