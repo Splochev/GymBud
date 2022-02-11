7e6dd383b8bd1ed6efffc03e4da6dbcf677f53a8
Stanislav Plochev <stanislav.plochev@gmail.com>
2022-02-11 12:40:55 +0200

    remove repetitive html elements

--- client/src/components/header/LoggedInHeader/LoggedInHeader.js
+++ client/src/components/header/LoggedInHeader/LoggedInHeader.js
@@ -72,7 +72,6 @@ const LoggedInHeader = () => {
     const [showCalorieCalculator, setShowCalorieCalculator] = useState(false);
     const { tab } = useQuery()
 
-
     useEffect(() => {
         if (tab) {
             switch (tab) {
@@ -102,7 +101,7 @@ const LoggedInHeader = () => {
     }
 
     return (
-        <div className="header">
+        <div className={clsx("nav-menu-wrapper", styles.blackStripe)}>
             <UGBModal
                 open={showOneRMCalculator}
                 onClose={() => {
@@ -133,123 +132,154 @@ const LoggedInHeader = () => {
             >
                 <CalorieCalculator />
             </UGBModal>
-            <div className="nav-wrapper">
-                <ul className="nav justify-content-end">
-                    <li className="nav-item">
-                        <a className={clsx("nav-link", styles.signInOrUpUrls)} href="#!">My Profile</a>
-                    </li>
-                    <li className="nav-item" onClick={onLogout}>
-                        <a className={clsx("nav-link", styles.signInOrUpUrls)} href="#!" onClick={onLogout}>Logout</a>
-                    </li>
-                    <li className="nav-item dropdown dropleft">
-                        <a className={clsx('nav-link dropdown-toggle', styles.questionIcon, styles.signInOrUpUrls)} href="#!" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i className="far fa-question-circle" />
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className={clsx("dropdown-item", styles.dropDown)} href="#!">Opt1</a>
-                            <a className={clsx("dropdown-item", styles.dropDown)} href="#!">Opt2</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div className={clsx("nav-menu-wrapper", styles.blackStripe)}>
-                <nav className={clsx("navbar navbar-expand-lg", styles.nav)}>
-                    <a className="navbar-brand" href="#!" onClick={(e) => {
+            <nav className={clsx("navbar navbar-expand-lg", styles.nav)}>
+                <a
+                    className="navbar-brand"
+                    href="#!"
+                    onClick={(e) => {
                         e.preventDefault();
                         history.push('/home');
-                    }}>
-                        <img src="/UrGymBudLogoLight.png" alt="Logo" className={styles.logo}></img>
-                    </a>
-                    <button className={clsx("navbar-toggler", styles.navToggler)} type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="bars-icon"><i className="fas fa-bars" /> </span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-                                ><i className="fas fa-hamburger"></i> <i className="fas fa-search" /></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className={clsx("nav-link", styles.navUrls)} href="#!">Meal Planner
-                                    <span className={clsx("badge badge-pill badge-secondary", styles.addedFoodCounter)}>0</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className={clsx("nav-link", styles.navUrls)} href="#!" onClick={(e) => {
+                    }}
+                >
+                    <img src="/UrGymBudLogoLight.png" alt="Logo" className={styles.logo}></img>
+                </a>
+                <button
+                    className={clsx("navbar-toggler", styles.navToggler)}
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                >
+                    <i className="fas fa-bars" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                                <i className="fas fa-hamburger" /> <i className="fas fa-search" />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                            >
+                                Meal Planner
+                                <span className={clsx("badge badge-pill badge-secondary", styles.addedFoodCounter)}>
+                                    0
+                                </span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                                onClick={(e) => {
                                     e.preventDefault();
                                     history.push('/progress-tracker');
-                                }}>Progress-Tracker</a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className={clsx("nav-link", styles.navUrls)} href="#!" onClick={(e) => {
-                                    e.preventDefault();
-                                    history.push({
-                                        search: "?tab=calorie-calculator",
-                                        state: { fromPopup: true }
-                                    });
-
                                 }}
-                                >Calorie
-                                    Calculator</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className={clsx("nav-link", styles.navUrls)} href="#!" onClick={(e) => {
+                            >
+                                Progress-Tracker
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                                onClick={(e) => {
                                     e.preventDefault();
-                                    history.push({
-                                        search: "?tab=one-rep-max-calculator",
-                                        state: { fromPopup: true }
-                                    });
-                                }}>
-                                    1 Rep
-                                    Max
-                                    Calculator
-                                </a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className={clsx("nav-link dropdown-toggle", styles.navUrls)} href="#!" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Food
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Food</a>
-                                    <a className={clsx("dropdown-item", styles.dropDown)} href="#!" onClick={(e) => {
+                                    history.push({ search: "?tab=calorie-calculator", state: { fromPopup: true } });
+                                }}
+                            >
+                                Calorie Calculator
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    history.push({ search: "?tab=one-rep-max-calculator", state: { fromPopup: true } });
+                                }}
+                            >
+                                1 Rep Max Calculator
+                            </a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a
+                                className={clsx("nav-link dropdown-toggle", styles.navUrls)}
+                                href="#!"
+                                role="button"
+                                data-toggle="dropdown"
+                            >
+                                Food
+                            </a>
+                            <div className="dropdown-menu" >
+                                <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Food</a>
+                                <a
+                                    className={clsx("dropdown-item", styles.dropDown)}
+                                    href="#!"
+                                    onClick={(e) => {
                                         e.preventDefault();
-                                        history.push({
-                                            search: "?tab=add-food",
-                                            state: { fromPopup: true }
-                                        });
-                                    }}>
-                                        Add Food
-                                    </a>
-                                </div>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className={clsx("nav-link dropdown-toggle", styles.navUrls)} href="#!" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Meals
+                                        history.push({ search: "?tab=add-food", state: { fromPopup: true } });
+                                    }}
+                                >
+                                    Add Food
                                 </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Meals</a>
-                                    <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Meal Plans</a>
-                                </div>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className={clsx("nav-link dropdown-toggle", styles.navUrls)} href="#!" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Workout Journal
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Workout Journal</a>
-                                    <a className={clsx("dropdown-item", styles.dropDown)} href="#!">Create A Workout Journal</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+                            </div>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a
+                                className={clsx("nav-link dropdown-toggle", styles.navUrls)}
+                                href="#!"
+                                role="button"
+                                data-toggle="dropdown"
+                            >
+                                Meals
+                            </a>
+                            <div className="dropdown-menu">
+                                <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Meals</a>
+                                <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Meal Plans</a>
+                            </div>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a
+                                className={clsx("nav-link dropdown-toggle", styles.navUrls)}
+                                href="#!"
+                                role="button"
+                                data-toggle="dropdown"
+                            >
+                                Workout Journal
+                            </a>
+                            <div className="dropdown-menu">
+                                <a className={clsx("dropdown-item", styles.dropDown)} href="#!">My Workout Journal</a>
+                                <a className={clsx("dropdown-item", styles.dropDown)} href="#!">Create A Workout Journal</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul className="nav justify-content-end">
+                        <li className="nav-item">
+                            <a className={clsx("nav-link", styles.navUrls)} href="#!">My Profile</a>
+                        </li>
+                        <li className="nav-item" onClick={onLogout}>
+                            <a className={clsx("nav-link", styles.navUrls)} href="#!" onClick={onLogout}>Logout</a>
+                        </li>
+                        <li className="nav-item dropdown dropleft">
+                            <a
+                                className={clsx('nav-link dropdown-toggle', styles.questionIcon, styles.navUrls)}
+                                href="#!"
+                                role="button"
+                                data-toggle="dropdown"
+                            >
+                                <i className="far fa-question-circle" />
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a className={clsx("dropdown-item", styles.dropDown)} href="#!">Opt1</a>
+                                <a className={clsx("dropdown-item", styles.dropDown)} href="#!">Opt2</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div >
     );
 }
--- client/src/components/header/LoggedOutHeader/LoggedOutHeader.js
+++ client/src/components/header/LoggedOutHeader/LoggedOutHeader.js
@@ -11,20 +11,11 @@ import { useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useQuery } from '../../utils/RouteUtils.js';
 
-
 const useStyles = makeStyles((theme) => ({
     questionIcon: {
         textDecoration: 'none',
         '&:hover': {
             textDecoration: 'none'
-        }
-    },
-    signInOrUpUrls: {
-        color: '#343a40',
-        fontSize: '20px',
-        '&:hover': {
-            textDecoration: 'underline',
-            color:'#007BFF'
         }
     },
     questionIconDropDown: {
@@ -64,12 +55,10 @@ const LoggedOutHeader = () => {
     const styles = useStyles();
     const history = useHistory();
     const [showForgotPassword, setShowForgotPassword] = useState(false);
-
     const [showOneRMCalculator, setShowOneRMCalculator] = useState(false);
     const [showCalorieCalculator, setShowCalorieCalculator] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
     const [showRegister, setShowRegister] = useState(false);
-
     const { tab } = useQuery();
 
     useEffect(() => {
@@ -97,7 +86,7 @@ const LoggedOutHeader = () => {
     }, [tab])
 
     return (
-        <div className="header">
+        <div className={styles.blackStripe}>
             <UGBModal
                 open={showOneRMCalculator}
                 onClose={() => {
@@ -151,84 +140,99 @@ const LoggedOutHeader = () => {
             >
                 <ForgotPassword />
             </UGBModal>
-            <div className="nav-wrapper">
-                <ul className="nav justify-content-end">
-                    <li className="nav-item">
-                        <a className={clsx("nav-link", styles.signInOrUpUrls)} href="#!" data-toggle="modal" data-target="#login-modal" onClick={(e) => {
-                            e.preventDefault();
-                            history.push({
-                                search: "?tab=sign-in",
-                                state: { fromPopup: true }
-                            });
-                        }}>
-                            Sign In
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className={clsx("nav-link", styles.signInOrUpUrls)} href="#!" onClick={(e) => {
-                            e.preventDefault();
-                            history.push({
-                                search: "?tab=sign-up",
-                                state: { fromPopup: true }
-                            });
-                        }}>Sign Up</a>
-                    </li>
-                    <li className="nav-item dropdown dropleft">
-                        <a className={clsx('nav-link dropdown-toggle', styles.questionIcon, styles.signInOrUpUrls)} href="#!" role="button" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            <i className="far fa-question-circle"></i>
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className={clsx("dropdown-item", styles.questionIconDropDown)} href="#!">Opt1</a>
-                            <a className={clsx("dropdown-item", styles.questionIconDropDown)} href="#!">Opt2</a>
-                        </div>
-                    </li>
-
-                </ul>
-            </div>
-            <div className={clsx("nav-menu-wrapper", styles.blackStripe)}>
-                <nav className={clsx("navbar navbar-expand-lg ", styles.nav)}>
-                    <a className="navbar-brand" href="#!" onClick={(e) => {
+            <nav className={clsx("navbar navbar-expand-lg", styles.nav)}>
+                <a
+                    className="navbar-brand"
+                    href="#!"
+                    onClick={(e) => {
                         e.preventDefault();
                         history.push('/home');
                     }}>
-                        <img src="/UrGymBudLogoLight.png" alt="Logo" className={styles.logo}></img>
-                    </a>
-                    <button className={clsx("navbar-toggler", styles.navToggler)} type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="bars-icon"><i className="fas fa-bars"></i></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
-                                    className="fas fa-hamburger"></i> <i className="fas fa-search"></i></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className={"nav-link " + styles.navUrls} href="#!" onClick={(e) => {
+                    <img src="/UrGymBudLogoLight.png" alt="Logo" className={styles.logo}></img>
+                </a>
+                <button
+                    className={clsx("navbar-toggler", styles.navToggler)}
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                >
+                    <i className="fas fa-bars" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+                                <i className="fas fa-hamburger" /> <i className="fas fa-search" />
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                                onClick={(e) => {
                                     e.preventDefault();
-                                    history.push({
-                                        search: "?tab=calorie-calculator",
-                                        state: { fromPopup: true }
-                                    });
-                                }}>Calorie Calculator</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className={"nav-link " + styles.navUrls} href="#!" onClick={(e) => {
+                                    history.push({ search: "?tab=calorie-calculator", state: { fromPopup: true } });
+                                }}
+                            >
+                                Calorie Calculator
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                                onClick={(e) => {
                                     e.preventDefault();
-                                    history.push({
-                                        search: "?tab=one-rep-max-calculator",
-                                        state: { fromPopup: true }
-                                    });
-                                }}>1 Rep
-                                    Max
-                                    Calculator
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+                                    history.push({ search: "?tab=one-rep-max-calculator", state: { fromPopup: true } });
+                                }}
+                            >
+                                1 Rep Max Calculator
+                            </a>
+                        </li>
+                    </ul>
+                    <ul className="nav justify-content-end">
+                        <li className="nav-item">
+                            <a
+                                className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                                data-toggle="modal"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    history.push({ search: "?tab=sign-in", state: { fromPopup: true } });
+                                }}
+                            >
+                                Sign In
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className={clsx("nav-link", styles.navUrls)}
+                                href="#!"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    history.push({ search: "?tab=sign-up", state: { fromPopup: true } });
+                                }}
+                            >
+                                Sign Up
+                            </a>
+                        </li>
+                        <li className="nav-item dropdown dropleft">
+                            <a
+                                className={clsx('nav-link dropdown-toggle', styles.questionIcon, styles.navUrls)}
+                                href="#!"
+                                role="button"
+                                data-toggle="dropdown"
+                            >
+                                <i className="far fa-question-circle" />
+                            </a>
+                            <div className="dropdown-menu">
+                                <a className={clsx("dropdown-item", styles.questionIconDropDown)} href="#!">Opt1</a>
+                                <a className={clsx("dropdown-item", styles.questionIconDropDown)} href="#!">Opt2</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 }
