192813f42996c2835eec0766e4735907cae4f4d5
Stanislav Plochev <stanislav.plochev@gmail.com>
2022-02-16 15:00:57 +0200

    add custom delimiter for dateParser

--- client/src/components/utils/utilFunc.js
+++ client/src/components/utils/utilFunc.js
@@ -1,9 +1,9 @@
-export function parseDate(date) {
+export function parseDate(date,delimiter='-') {
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
     if (!year || !month || !day) {
         return ''
     }
-    return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`
+    return `${year}${delimiter}${month < 10 ? `0${month}` : month}${delimiter}${day < 10 ? `0${day}` : day}`
 }
